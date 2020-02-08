c2dc815b68da75c531a672790fdda4f8a9dab18c
TprceOYX <1634834938@qq.com>
2020-02-08 20:17:47 +0800

    update

--- Demo/gensim_test.py
+++ Demo/gensim_test.py
@@ -33,9 +33,9 @@ if __name__ == '__main__':
     inp, outp1, outp2 = sys.argv[1:4]
     '''
     LineSentence(inp)：格式简单：一句话=一行; 单词已经过预处理并被空格分隔。
-    size：是每个词的向量维度； 
-    window：是词向量训练时的上下文扫描窗口大小，窗口为5就是考虑前5个词和后5个词； 
-    min-count：设置最低频率，默认是5，如果一个词语在文档中出现的次数小于5，那么就会丢弃； 
+    size：是每个词的向量维度；
+    window：是词向量训练时的上下文扫描窗口大小，窗口为5就是考虑前5个词和后5个词；
+    min-count：设置最低频率，默认是5，如果一个词语在文档中出现的次数小于5，那么就会丢弃；
     workers：是训练的进程数（需要更精准的解释，请指正），默认是当前运行机器的处理器核数。这些参数先记住就可以了。
     sg ({0, 1}, optional) – 模型的训练算法: 1: skip-gram; 0: CBOW
     alpha (float, optional) – 初始学习率
@@ -49,4 +49,3 @@ if __name__ == '__main__':
     model.save(outp1)
     # 不以C语言可以解析的形式存储词向量
     model.wv.save_word2vec_format(outp2, binary=False)
-    # python gensim_test.py wiki_jian_zh_seg-189.5.txt wiki_zh_jian_text.model wiki_zh_jian_text.vector
--- read_mongo/read_data.py
+++ read_mongo/read_data.py
@@ -1,10 +1,40 @@
+'''
+用于读取数据库数据，使用jieba分词后保存为txt文件
+'''
 import pymongo
-# 连接数据库
-myclient = pymongo.MongoClient(
-    "mongodb://tprce:1634834938@47.94.0.240:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false"
-)
-mydb = myclient["document"]
-mycol = mydb["baike"]
-myresult = mycol.find({}, {"_id": 0, "text": 1}).limit(1)
-for x in myresult:
-    print(type(x["text"]))
+import codecs
+import jieba.analyse
+
+
+def cut_text(text, stop_words):
+    target = codecs.open("./train_data/" + str(text["_id"]) + ".txt",
+                         'w',
+                         encoding="utf8")
+    words = jieba.cut(text["text"], cut_all=False)
+    line = ""
+    # 去除标点符号
+    for word in words:
+        if word not in stop_words:
+            line += word
+            line += " "
+    target.writelines(line)
+    target.close()
+
+
+def process_data():
+    # 连接数据库
+    myclient = pymongo.MongoClient(
+        "mongodb://tprce:1634834938@47.94.0.240:27017/")
+    mydb = myclient["document"]
+    mycol = mydb["baike"]
+    myresult = mycol.find({}, {"_id": 1, "text": 1}).limit(1000)
+    # 加载停用词，如标点符号等
+    stop_file = codecs.open("./data/stop_words.txt", "r", encoding="utf8")
+    stop_words = stop_file.read().split("\n")
+    jieba.set_dictionary("./data/dict.txt.big")
+    for x in myresult:
+        cut_text(x, stop_words)
+
+
+if __name__ == "__main__":
+    process_data()
