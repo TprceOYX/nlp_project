a81ffb5ff5e6b6a29c3cba568c1d582d03f21727
TprceOYX <1634834938@qq.com>
2020-05-06 16:53:42 +0800

    update

--- test/test_model.py
+++ test/test_model.py
@@ -2,6 +2,14 @@ from gensim import models
 
 w2v = models.KeyedVectors.load_word2vec_format("./models/final/word2Vec2.model", binary=True)
 # 输入一个词找出相似的前10个词
-one_corpus = ["番茄"]
+one_corpus = ["番茄", "亚洲", "编程", "企业", "地理"]
 result = w2v.most_similar(one_corpus[0], topn=10)
+print(result)
+result = w2v.most_similar(one_corpus[1], topn=10)
+print(result)
+result = w2v.most_similar(one_corpus[2], topn=10)
+print(result)
+result = w2v.most_similar(one_corpus[3], topn=10)
+print(result)
+result = w2v.most_similar(one_corpus[4], topn=10)
 print(result)
